231c1f323cd0b9d7c71489c28e93856cfd31365c
Joudrier Kevin <kev.joudrier@gmail.com>
2020-12-04 19:03:09 +0100

    Configuration du projet avec webpack et eslint.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+
+    mode: "development",
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath:"/dist/",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            // BABEL
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {presets: ["@babel/env"]}
+            },
+
+            // CSS in the header
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    devServer: {
+        contentBase: path.join(__dirname, "dist/"),
+        port:3000,
+        publicPath:"http://localhost:3000/dist",
+        hot: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin(), new EslintWebpackPlugin()]
+}
